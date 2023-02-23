9947139b1f7e7258a091413b3995e93b5620817b
lindsayRae <lbarnett712@gmial.com>
2023-02-23 05:45:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function GoalForm(props) {
+  const [formData, setFormData] = useState({ goal: '', by: '' });
+
+  function changeHandler(e) {
+    console.log(e.target);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    // spread operator makes a copy, keeping state immutable
+    // using brackets allows me to set the value of the E target named dynamically
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    props.onAdd(formData);
+    setFormData({ goal: '', by: '' });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>My Goals</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type='text'
+          name='goal'
+          placeholder='Goal'
+          value={formData.goal}
+          onChange={changeHandler}
+        />
+        <input
+          type='text'
+          name='by'
+          placeholder='By...'
+          value={formData.by}
+          onChange={changeHandler}
+        />
+        <button>Submit Goal</button>
+      </form>
+    </>
   );
 }
 
-export default App;
+function ListOfGoals(props) {
+  return (
+    <ul>
+      {props.allGoals.map((g) => (
+        <li key={g.goal}>
+          <span>
+            My goal is to {g.goal}, by {g.by}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function App() {
+  const [allGoals, updateAllGoals] = useState([]);
+
+  function addGoal(goal) {
+    updateAllGoals([...allGoals, goal]);
+  }
+
+  return (
+    <div className='App'>
+      <GoalForm onAdd={addGoal}></GoalForm>
+      <ListOfGoals allGoals={allGoals}></ListOfGoals>
+    </div>
+  );
+}
